1ce53930fecfa6ac3791f16b112a51df53a9cc90
“Marcin <m.koska2@wp.pl>
2020-02-14 16:50:26 +0100

    Add number of posts

--- bieszczady-widget-recent-posts.php
+++ bieszczady-widget-recent-posts.php
@@ -33,7 +33,7 @@ class bsd_widget extends WP_Widget {
  
 public function widget( $args, $instance ) {
     $title = apply_filters( 'widget_title', $instance['title'] );
-    $number = apply_filters( 'widget_title', $instance['number'] );
+    $number = apply_filters( 'widget_custom_html_content', $instance['number'] );
     
  
     // before and after widget arguments are defined by themes
